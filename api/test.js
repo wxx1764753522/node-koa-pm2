3c79f52991697fcfaaf2a9d953ca12f94ada2cca
wxx1764753522 <1764753522@qq.com>
2020-07-24 01:20:50 +0800

    publish

---
+++ api/test.js
@@ -0,0 +1,20 @@
+const { query } = require('koa/lib/request');
+
+const Router = require('koa-router'),
+    router = new Router();
+router.get('/api/', (ctx, next) => {
+    ctx.body = 'Hello World';
+}).get('/api/test', (ctx, next) => {
+    ctx.body = {
+        msg: 'here is test',
+        query: ctx.query,
+        queryStr: ctx.querystring
+    }
+}).post('/api/users', (ctx, next) => {
+    ctx.body = 'here is users';
+}).all('/api/users/:id', (ctx, next) => {
+    ctx.body = `here is users${ctx.params.id}`;
+})
+
+
+module.exports = router
---
+++ app.js
@@ -0,0 +1,17 @@
+const Koa = require('koa'),
+    app = new Koa(),
+    router = require('./api/test');
+
+
+app.use(async (ctx, next) => {
+    const start = Date.now();
+
+    await next();
+
+    const ms = Date.now() - start;
+
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+})
+
+app.use(router.routes())
+app.listen(3000)
